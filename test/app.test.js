edd9ba891a6446c3795db2ee934255ced565d408
Jackmc <jackmc1122@gmail.com>
2019-07-17 09:31:20 -0700

    Add a GET route to get all people.

--- test/app.test.js
+++ test/app.test.js
@@ -23,4 +23,20 @@ describe('app routes', () => {
         });
       });
   });
+
+  it('returns a list of all people', () => {
+    return request(app)
+      .get('/api/v1/people')
+      .then(res => {
+        expect(res.body).toEqual(expect.any(Array));
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          name: expect.any(String),
+          email: expect.any(String),
+          city: expect.any(String),
+          state: expect.any(String),
+          __v: 0
+        });
+      });
+  });
 });
