9c7b86eb65d102a6aee6c287e08302da6076815e
Jackmc <jackmc1122@gmail.com>
2019-07-17 11:28:19 -0700

    Seed a dog in the DB for tests.

--- test/seedData.js
+++ test/seedData.js
@@ -1,4 +1,5 @@
 const Person = require('../lib/models/Person');
+const Dog = require('../lib/models/Dog');
 
 const seedPeople = [
   {
@@ -9,11 +10,25 @@ const seedPeople = [
   }
 ];
 
+const seedDogs = [
+  {
+    name: 'Bear',
+    age: '13',
+    weight: '40 lbs',
+  }
+];
+
 function seedData() {
-  return Promise.all(seedPeople.map(meme => {
-    const { name, email, city, state, profileImage } = meme;
+  return Promise.all(seedPeople.map(person => {
+    const { name, email, city, state, profileImage } = person;
     return Person.create({ name, email, city, state, profileImage });
-  }));
+  }))
+    .then(person => {
+      return Promise.all(seedDogs.map(dog => {
+        const { name, age, weight } = dog;
+        return Dog.create({ name, age, weight, owner: person[0]._id });
+      }));
+    });
 }
 
 module.exports = seedData;
